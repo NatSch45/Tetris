fc49beecb4912054ae0004b17cc3a6483ba2e43b
NMELLAL <nathy.mellal@ynov.com>
2021-11-25 15:22:30 +0100

    start CSS/Nav

--- BlazorApp/Startup.cs
+++ BlazorApp/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorApp.Data;
+
 
 namespace BlazorApp
 {
@@ -28,7 +28,7 @@ namespace BlazorApp
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
